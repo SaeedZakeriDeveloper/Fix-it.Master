e7e41787cbfb69d279378370b99d38da9582e428
saeedzakeri1364 <saeed.zakeri1364@gmail.com>
2023-08-02 22:02:01 -0400

    test

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 
 
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index_fr.html'));
 });
+
 
 const port = 3000; 
 app.listen(port, () => {
