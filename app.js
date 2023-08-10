5970a02ab6c0c56e885199e1d0ace694c1fbb31f
saeedzakeri1364 <saeed.zakeri1364@gmail.com>
2023-08-10 15:13:52 -0400

    edit loader

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.post('/submit_form',(req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const port = process.env.PORT || 82;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
